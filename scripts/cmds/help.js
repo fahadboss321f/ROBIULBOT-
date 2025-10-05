89998f6741d8c6d0fbfc3668d274c68d1e730547
fahadboss321f <causebossanothergamingboy@gmail.com>
2025-10-05 11:59:44 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[🆁🅾🅱🅸🆄🅻]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[FAHAD]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
   config: {
